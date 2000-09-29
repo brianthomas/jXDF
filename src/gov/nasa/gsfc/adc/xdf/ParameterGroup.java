532a2bd889595b183e3bb900965d9b5b2717007f
cvs <>
2000-09-29 22:10:45 +0000

    new file

---
+++ src/gov/nasa/gsfc/adc/xdf/ParameterGroup.java
@@ -0,0 +1,13 @@
+package gov.nasa.gsfc.adc.xdf;
+
+import java.util.*;
+
+// CVS $Id$
+
+/**
+ * Structure.java
+ * @author: Kelly Zeng (kelly.zeng@commerceone.com)
+ */
+
+ public class ParameterGroup {
+ }
