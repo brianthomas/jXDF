8184e0d95e35e0c8a169022b94034c8a61598c1b
kelly <>
2000-10-10 19:04:59 +0000

    first stab

---
+++ src/gov/nasa/gsfc/adc/xdf/FieldAxis.java
@@ -0,0 +1,23 @@
+// XDF FieldAxis Class
+// CVS $Id$
+
+package gov.nasa.gsfc.adc.xdf;
+
+import java.util.*;
+
+// FieldAxis.java Copyright (C) 2000 Brian Thomas,
+// ADC/GSFC-NASA, Code 631, Greenbelt MD, 20771
+
+public class FieldAxis extends BaseObject{
+
+   public List getDataFormatList() {
+     Log.error("in FieldAxis, getDataFormatList(), function body empty, returning null");
+     return null;
+  }
+
+  public String getAxisId() {
+    Log.error("in FieldAxis, getAxisId(), function body empty, currently returning null");
+    return null;
+  }
+
+}
