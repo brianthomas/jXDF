db382846bfc77110ac7ba2830bd9b9c8fa1f0a36
kelly <>
2000-10-11 18:40:14 +0000

    first layout of the class

---
+++ src/gov/nasa/gsfc/adc/xdf/UnitDirection.java
@@ -0,0 +1,24 @@
+// XDF UnitDirection
+// CVS $Id$
+
+package gov.nasa.gsfc.adc.xdf;
+
+/** UnitDirection
+ * @version $Revision$
+  */
+
+public class UnitDirection extends BaseObject{
+
+   public UnitDirection () {
+
+   }
+
+}
+
+/* Modification History:
+ *
+ * $Log$
+ * Revision 1.1  2000/10/11 18:40:14  kelly
+ * first layout of the class
+ *
+ */
