ccfab4fc038a73ebb268673ce262f5210fadec2b
kelly <>
2000-10-11 19:05:53 +0000

    created the class.

---
+++ src/gov/nasa/gsfc/adc/xdf/TaggedXMLDataIOStyle.java
@@ -0,0 +1,23 @@
+// XDF TaggedXMLDataIOStyle Class
+// CVS $Id$
+
+package gov.nasa.gsfc.adc.xdf;
+
+import java.util.*;
+
+// TaggedXMLDataIOStyle.java Copyright (C) 2000 Brian Thomas,
+// ADC/GSFC-NASA, Code 631, Greenbelt MD, 20771
+
+public class TaggedXMLDataIOStyle extends XMLDataIOStyle {
+  public TaggedXMLDataIOStyle() {
+    Log.debug("in TaggedXMLDataIOStyle, no-arg constructor");
+  }
+
+}
+/* Modification History:
+ *
+ * $Log$
+ * Revision 1.1  2000/10/11 19:05:53  kelly
+ * created the class.
+ *             
+ */
