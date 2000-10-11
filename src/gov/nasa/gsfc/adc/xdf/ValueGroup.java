6cdd7f57dc8674f9ad4ec8d96a594413ef980c9f
kelly <>
2000-10-11 18:41:08 +0000

    created the class

---
+++ src/gov/nasa/gsfc/adc/xdf/ValueGroup.java
@@ -0,0 +1,25 @@
+// XDF ValueGroup Class
+// CVS $Id$
+
+// ValueGroup.java Copyright (C) 2000 Brian Thomas,
+// ADC/GSFC-NASA, Code 631, Greenbelt MD, 20771
+
+package gov.nasa.gsfc.adc.xdf;
+import java.util.*;
+
+/** ValueGroup.java:
+   @version $Revision$
+*/
+
+public class ValueGroup extends BaseObject{
+  public ValueGroup() {
+  }
+}
+
+/* Modification History:
+ *
+ * $Log$
+ * Revision 1.1  2000/10/11 18:41:08  kelly
+ * created the class
+ *
+ */
