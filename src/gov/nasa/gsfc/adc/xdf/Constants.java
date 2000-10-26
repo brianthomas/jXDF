be4f8c842528e467020c9415c3d4517833eb6b87
kelly <>
2000-10-26 20:10:38 +0000

    added NUMBER_OR_STRING_TYPE, enum lists are now String[].  it is faster.  -k.z.

--- src/gov/nasa/gsfc/adc/xdf/Constants.java
+++ src/gov/nasa/gsfc/adc/xdf/Constants.java
@@ -14,13 +14,15 @@ public abstract class Constants {
   public static final String LIST_TYPE   = "List";
   public static final String OBJECT_TYPE = "Object";
   public static final String NUMBER_TYPE = "Number";
+  public static final String STRING_OR_NUMBER_TYPE = "StringOrNumber";
   //store the enum list of XMLattributeTypes
-  public static final List XMLATTRIBUTE_TYPE_LIST = new ArrayList();
+  public static final String[] XMLATTRIBUTE_TYPE_LIST = new String[5];
   static {
-    XMLATTRIBUTE_TYPE_LIST.add(STRING_TYPE);
-    XMLATTRIBUTE_TYPE_LIST.add(LIST_TYPE);
-    XMLATTRIBUTE_TYPE_LIST.add(OBJECT_TYPE );
-    XMLATTRIBUTE_TYPE_LIST.add(NUMBER_TYPE);
+    XMLATTRIBUTE_TYPE_LIST[0]=STRING_TYPE;
+    XMLATTRIBUTE_TYPE_LIST[1]= LIST_TYPE;
+    XMLATTRIBUTE_TYPE_LIST[2] = OBJECT_TYPE ;
+    XMLATTRIBUTE_TYPE_LIST[3] = NUMBER_TYPE;
+    XMLATTRIBUTE_TYPE_LIST [4] = STRING_OR_NUMBER_TYPE;
   }
 
   public static final String NEW_LINE = System.getProperty("line.separator");
@@ -29,21 +31,21 @@ public abstract class Constants {
   public static final String BIG_ENDIAN = "BigEndian";
   public static final String LITTLE_ENDIAN = "LittleEndian";
   //store the enum list of endians
-  public static final List ENDIANS_LIST =  new ArrayList();
+  public static final String[] ENDIANS_LIST =  new String[2];
   static {
-    ENDIANS_LIST.add(BIG_ENDIAN);
-    ENDIANS_LIST.add(LITTLE_ENDIAN);
+    ENDIANS_LIST[0] = BIG_ENDIAN;
+    ENDIANS_LIST[1] = LITTLE_ENDIAN;
   }
 
   public static final String INTEGER_TYPE_DECIMAL = "decimal";
   public static final String INTEGER_TYPE_HEX = "hexadecimal";
   public static final String INTEGER_TYPE_OCTAL = "octal";
   //store the enum list of integer types
-  public static final List INTEGER_TYPE_LIST = new ArrayList();
+  public static final String[] INTEGER_TYPE_LIST = new String[3];
   static {
-    INTEGER_TYPE_LIST.add(INTEGER_TYPE_DECIMAL);
-    INTEGER_TYPE_LIST.add(INTEGER_TYPE_HEX);
-    INTEGER_TYPE_LIST.add(INTEGER_TYPE_OCTAL);
+    INTEGER_TYPE_LIST[0]=INTEGER_TYPE_DECIMAL;
+    INTEGER_TYPE_LIST[1]=INTEGER_TYPE_HEX;
+    INTEGER_TYPE_LIST[2]=INTEGER_TYPE_OCTAL;
   }
 
   public static final String UTF_8 = "UTF-8";
@@ -51,12 +53,30 @@ public abstract class Constants {
   public static final String ISO_8859_1 = "ISO-8859-1";
   public static final String ANSI = "ANSI";
   //store the enum list of encodings
-  public static final List  ENCODINGS_LIST = new ArrayList();
+  public static final String[] ENCODINGS_LIST = new String[4];
   static {
-    ENCODINGS_LIST.add(UTF_8);
-    ENCODINGS_LIST.add(UTF_16);
-    ENCODINGS_LIST.add(ISO_8859_1 );
-    ENCODINGS_LIST.add(ANSI);
+    ENCODINGS_LIST[0]=UTF_8;
+    ENCODINGS_LIST[1]=UTF_16;
+    ENCODINGS_LIST[2]=ISO_8859_1 ;
+    ENCODINGS_LIST[3]=ANSI;
   }
+
+  public static final String DATATYPE_INTEGER = "integer";
+  public static final String DATATYPE_FIXED = "fixed";
+  public static final String DATATYPE_EXPONENTIAL = "expenential";
+  public static final String DATATYPE_STRING = "string";
+  public static final String DATATYPE_URL = "url";
+  public static final String[] DATATYPE_LIST = new String[5];
+
+  static {
+    DATATYPE_LIST[0]=DATATYPE_INTEGER;
+    DATATYPE_LIST[1]=DATATYPE_FIXED;
+    DATATYPE_LIST[2]=DATATYPE_EXPONENTIAL;
+    DATATYPE_LIST[3]=DATATYPE_STRING;
+    DATATYPE_LIST[4]=DATATYPE_URL;
+  }
+
+
+
 
 }
