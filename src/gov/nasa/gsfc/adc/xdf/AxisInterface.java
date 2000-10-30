dee94129ed1a86fd9ae7f79c27a861ad84f4759a
kelly <>
2000-10-30 18:21:09 +0000

    created the class for the common signature of Axis and FieldAxis.  -k.z.

---
+++ src/gov/nasa/gsfc/adc/xdf/AxisInterface.java
@@ -0,0 +1,13 @@
+package gov.nasa.gsfc.adc.xdf;
+public interface AxisInterface {
+  public int getLength();
+  public String getAxisId();
+}
+
+/* Modification History:
+ *
+ * $Log$
+ * Revision 1.1  2000/10/30 18:21:09  kelly
+ * created the class for the common signature of Axis and FieldAxis.  -k.z.
+ *
+ */
