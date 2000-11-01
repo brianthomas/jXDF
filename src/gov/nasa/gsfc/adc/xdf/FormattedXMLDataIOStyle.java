78138007f2117e5bc6f4ae4f0a512f657331ad68
kelly <>
2000-11-01 16:04:59 +0000

    created the class

---
+++ src/gov/nasa/gsfc/adc/xdf/FormattedXMLDataIOStyle.java
@@ -0,0 +1,29 @@
+// XDF FormattedXMLDataIOStyle Class
+// CVS $Id$
+
+package gov.nasa.gsfc.adc.xdf;
+
+import java.util.*;
+import java.io.*;
+
+// FormattedXMLDataIOStyle.java Copyright (C) 2000 Brian Thomas,
+// ADC/GSFC-NASA, Code 631, Greenbelt MD, 20771
+
+
+/**TaggedXMLDatatIOStytle.java:
+ *  @version $Revision$
+ */
+
+public class FormattedXMLDataIOStyle extends XMLDataIOStyle {
+  protected void specificIOStyleToXDF(OutputStream out, String indent){
+
+  }
+}
+
+/* Modification History:
+ *
+ * $Log$
+ * Revision 1.1  2000/11/01 16:04:59  kelly
+ * created the class
+ *
+ */
