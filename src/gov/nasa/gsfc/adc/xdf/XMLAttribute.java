017756405b0bf6b3cf893a35e72b248e548ebf3e
kelly <>
2000-11-06 21:13:06 +0000

    the class is synchronized now.  -k.z.

--- src/gov/nasa/gsfc/adc/xdf/XMLAttribute.java
+++ src/gov/nasa/gsfc/adc/xdf/XMLAttribute.java
@@ -26,14 +26,14 @@ import java.util.ArrayList;
 
     /** Set the value of this XMLAttribute.
     */
-    public Object setAttribValue(Object objValue) {
+    public synchronized Object  setAttribValue(Object objValue) {
       attribValue = objValue;
       return attribValue;
     }
 
     /** Set the type of value held by this XMLAttribute.
     */
-    public String setAttribType(String strType) {
+    public synchronized String setAttribType(String strType) {
       if ( !Utility.isValidXMLAttributeType(strType))
       {
         Log.error("Type not a defined constant for XMLAttribute");
@@ -47,61 +47,54 @@ import java.util.ArrayList;
 
     /** Get the value of this XMLAttribute.
     */
-    public Object getAttribValue() {
+    public synchronized Object  getAttribValue() {
        return attribValue;
     }
 
     /** Get the XMLAttribute value type.
     */
-    public String getAttribType() {
+    public synchronized String  getAttribType() {
        return attribType;
     }
 
     public Object clone () throws CloneNotSupportedException{
 
-      XMLAttribute cloneObj = (XMLAttribute) super.clone();
-      // need to deep copy the fields here too
-      if (attribValue == null) {
+      synchronized (this) {
+        XMLAttribute cloneObj = null;cloneObj= (XMLAttribute) super.clone();
+          synchronized (cloneObj) {
+
+          // need to deep copy the fields here too
+          if (attribValue == null) {
+            return cloneObj;
+          }
+          if (attribValue instanceof String ) {
+            cloneObj.attribValue = new String((String) this.attribValue);
+            return cloneObj;
+         }
+         if (attribValue instanceof Integer) {
+            cloneObj.attribValue = new Integer(((Integer) this.attribValue).intValue());
+            return cloneObj;
+         }
+         if (attribValue instanceof Double) {
+          cloneObj.attribValue = new Double(((Double) this.attribValue).doubleValue());
+          return cloneObj;
+         }
+         if (attribValue instanceof List) {
+           cloneObj.attribValue = Collections.synchronizedList(new ArrayList(((List) this.attribValue).size()));
+           int stop = ((List)this.attribValue).size();
+           for (int i = 0; i < stop; i ++) {
+              //List only contains child classes of BaseObject
+              Object obj = ((List)this.attribValue).get(i);
+              ((List)cloneObj.attribValue).add(((BaseObject) obj).clone());
+
+            }
+            return cloneObj;
+         }
+        //all other classes are child classes of BaseObject
+        cloneObj.attribValue = ((BaseObject) this.attribValue).clone();
         return cloneObj;
       }
-      if (attribValue instanceof String ) {
-        cloneObj.attribValue = new String((String) this.attribValue);
-        return cloneObj;
-      }
-      if (attribValue instanceof Integer) {
-        cloneObj.attribValue = new Integer(((Integer) this.attribValue).intValue());
-        return cloneObj;
-      }
-      if (attribValue instanceof Double) {
-        cloneObj.attribValue = new Double(((Double) this.attribValue).doubleValue());
-        return cloneObj;
-      }
-
-
-      /**
-      if (attribValue instanceof Axis) {
-        cloneObj.attribValue =((Axis) this.attribValue).clone();
-      }
-      if (attribValue instanceof Units) {
-        cloneObj.attribValue =((Units) this.attribValue).clone();
-      }
-      */
-      if (attribValue instanceof List) {
-        cloneObj.attribValue = Collections.synchronizedList(new ArrayList(((List) this.attribValue).size()));
-        int stop = ((List)this.attribValue).size();
-        for (int i = 0; i < stop; i ++) {
-          //List only contains child classes of BaseObject
-          Object obj = ((List)this.attribValue).get(i);
-          ((List)cloneObj.attribValue).add(((BaseObject) obj).clone());
-
-        }
-        return cloneObj;
-      }
-
-       //all other classes are child classes of BaseObject
-       cloneObj.attribValue = ((BaseObject) this.attribValue).clone();
-       return cloneObj;
-
     }
+  }
 
-  } // end of internal Class XMLAttribute
+} // end of internal Class XMLAttribute
