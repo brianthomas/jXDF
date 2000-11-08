bc04ee8d1d6e75987bf2627c9cf031f028f1d696
thomas <>
2000-11-08 19:47:40 +0000

    Updated header to include GPL blurb. -b.t.

--- src/gov/nasa/gsfc/adc/xdf/FieldAxis.java
+++ src/gov/nasa/gsfc/adc/xdf/FieldAxis.java
@@ -1,5 +1,27 @@
+
 // XDF FieldAxis Class
 // CVS $Id$
+
+// FieldAxis.java Copyright (C) 2000 Brian Thomas,
+// ADC/GSFC-NASA, Code 631, Greenbelt MD, 20771
+
+/*
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+*/
+
 
 package gov.nasa.gsfc.adc.xdf;
 
@@ -10,9 +32,6 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Iterator;
-
-// FieldAxis.java Copyright (C) 2000 Brian Thomas,
-// ADC/GSFC-NASA, Code 631, Greenbelt MD, 20771
 
 
 public class FieldAxis extends BaseObject implements AxisInterface{
@@ -307,6 +326,9 @@ public class FieldAxis extends BaseObject implements AxisInterface{
  /**
   * Modification History:
   * $Log$
+  * Revision 1.9  2000/11/08 19:47:40  thomas
+  * Updated header to include GPL blurb. -b.t.
+  *
   * Revision 1.8  2000/11/06 21:16:34  kelly
   * added clone()  -k.z.
   *
