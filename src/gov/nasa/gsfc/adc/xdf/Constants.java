31be48e5b9cd99fb5777cc1dc6f65c2f2610ff2a
thomas <>
2000-11-08 19:36:32 +0000

    Trimmed import path; added GPL statement and Mod History. -b.t.

--- src/gov/nasa/gsfc/adc/xdf/Constants.java
+++ src/gov/nasa/gsfc/adc/xdf/Constants.java
@@ -1,7 +1,28 @@
+
 // XDF Constants Class
 // CVS $Id$
+
+// Constants.java Copyright (C) 2000 Brian Thomas,
+// ADC/GSFC-NASA, Code 631, Greenbelt MD, 20771
+
+/*
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+*/
+
 package gov.nasa.gsfc.adc.xdf;
-import java.util.*;
 
 /**
  * Constants.java:
@@ -125,4 +146,11 @@ public abstract class Constants {
 
 }
 
-
+/* Modification History
+ * 
+ * $Log$
+ * Revision 1.8  2000/11/08 19:36:32  thomas
+ * Trimmed import path; added GPL statement and Mod History. -b.t.
+ *
+ *
+ */
