0e882de86d2bb34583be918755851370ff143fa3
thomas <>
2001-09-24 15:18:05 +0000

    license update

--- src/gov/nasa/gsfc/adc/xdf/Axis.java
+++ src/gov/nasa/gsfc/adc/xdf/Axis.java
@@ -1,8 +1,26 @@
+
 // XDF Axis Class
 // CVS $Id$
 
 // Axis.java Copyright (C) 2000 Brian Thomas,
 // ADC/GSFC-NASA, Code 631, Greenbelt MD, 20771
+
+/*
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+*/
 
 package gov.nasa.gsfc.adc.xdf;
 
@@ -721,6 +739,9 @@ implements AxisInterface
 /* Modification History:
  *
  * $Log$
+ * Revision 1.29  2001/09/24 15:18:05  thomas
+ * license update
+ *
  * Revision 1.28  2001/09/19 17:51:32  thomas
  * made some set*List methods deprecated
  *
